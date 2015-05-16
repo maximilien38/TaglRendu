d8fd0ebdc5a00410e036f665e34f7a41eee5f8a6
maximilien38 <maximilien-faure@hotmail.fr>
2015-05-16 23:49:33 +0200

    archetype

---
+++ wiperdog-osgi-ipojo-archetype/src/main/resources/archetype-resources/src/main/java/SampleIntf.java
@@ -0,0 +1,9 @@
+/**
+ * iPOJO sample service interface.
+ * REMOVE THIS CLASS AS SOON AS YOU UNDERSTOOD WHAT THIS FILE GOING TO DO.
+ */
+package ${package};
+
+public interface SampleIntf {
+	String sayHello();
+}
---
+++ wiperdog-osgi-ipojo-archetype/src/main/resources/archetype-resources/src/main/java/internal/SampleProviderImpl.java
@@ -0,0 +1,15 @@
+/**
+ * iPOJO sample service provider.
+ * REMOVE THIS CLASS AS SOON AS YOU UNDERSTOOD WHAT THIS FILE GOING TO DO.
+ */
+package ${package}.internal;
+
+import ${package}.SampleIntf;
+
+public class SampleProviderImpl implements SampleIntf {
+
+	public String sayHello() {
+		return "Hello";
+	}
+
+}
